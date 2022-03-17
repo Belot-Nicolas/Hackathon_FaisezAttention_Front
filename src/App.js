46383abd7b54a62401b027f5a530c3e589ebaa19
Maud <mamaud2003@yahoo.fr>
2022-03-17 15:05:26 +0100

    dev update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 
 import { Route, Routes } from 'react-router-dom';
 import LoginDashboard from './components/LoginDashboard';
+import Register from '.'
+import Gameone from './components/Gameone'
 import './App.css';
-import Gameone from '../src/Gameone'
 import UserProvider from './contexts/UserProvider';
 
 function App() {
   return (
     <div className="App">
-      <Gameone />
+     
       <h1>Hello</h1>
       <UserProvider>
         <Routes>
           <Route path='/' element={<LoginDashboard />} />
-        </Routes>
-      </UserProvider>
-      <UserProvider>
-        <Routes>
-          <Route path='/' element={<LoginDashboard />} />
+          <Route path='/jeux' element={<Gameone />} />
+          <Route path='/admin' element={<Register />} />
         </Routes>
       </UserProvider>
     </div>
