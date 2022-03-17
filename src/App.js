8fdfa3d0fa6bb716f30e8a19845dc8e1fb891c57
Maud <mamaud2003@yahoo.fr>
2022-03-17 15:58:48 +0100

    dev eslint fixed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
 
 import { Route, Routes } from 'react-router-dom';
-import LoginDashboard from './components/LoginDashboard';
-import Register from '.'
-import Gameone from './components/Gameone'
-import './App.css';
-<<<<<<< HEAD
-=======
-import Gameone from './pages/Gameone'
->>>>>>> 9e734637d4f015c217eb46d96a260e18d3493bfb
-import UserProvider from './contexts/UserProvider';
+// import LoginDashboard from './components/LoginDashboard';
+// import Register from './components/Register';
+import Gameone from './components/Gameone';
 import logoBlanc from './assets/logoBlanc.png';
+import './App.css';
+
+import UserProvider from './contexts/UserProvider';
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-     
-      <h1>Hello</h1>
-      <UserProvider>
-        <Routes>
-          <Route path='/' element={<LoginDashboard />} />
-          <Route path='/jeux' element={<Gameone />} />
-          <Route path='/admin' element={<Register />} />
-=======
-      
       <h1><img className='Title' src={logoBlanc}/></h1>
+     
       <UserProvider>
         <Routes>
-          <Route path='/' element={<LoginDashboard />} />
-        
-          <Route path='/Game' element={<Gameone/>} />
->>>>>>> 9e734637d4f015c217eb46d96a260e18d3493bfb
+          {/* <Route path='/' element={<LoginDashboard />} /> */}
+          <Route path='jeux' element={<Gameone />} />
+          {/* <Route path='/admin' element={<Register />} /> */}
         </Routes>
       </UserProvider>
     </div>
