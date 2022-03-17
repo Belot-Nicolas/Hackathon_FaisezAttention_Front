870de9ef3f613ebb84068e2041ba31534b911411
Iysha Barton <iysha.barton@gmail.com>
2022-03-17 15:20:27 +0100

    Jeremie did some stuff

--- src/App.js
+++ src/App.js
@@ -2,28 +2,20 @@
 import { Route, Routes } from 'react-router-dom';
 import LoginDashboard from './components/LoginDashboard';
 import './App.css';
-<<<<<<< HEAD
-import Gameone from '../src/Gameone'
-=======
+import Gameone from './pages/Gameone'
 import UserProvider from './contexts/UserProvider';
->>>>>>> 6c23ceafd7a2956950743123605bbe4858f2b52f
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Gameone />
       
-      
-=======
       <h1>Hello</h1>
        <UserProvider>
           <Routes>
             <Route path ='/' element={<LoginDashboard/>} />
+            <Route path='/game' element={<Gameone/>} />
           </Routes> 
-
         </UserProvider>
->>>>>>> 6c23ceafd7a2956950743123605bbe4858f2b52f
     </div>
   );
 }
