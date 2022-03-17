bad20c095aecd6b87b67bf34d4b7d2254b96c3b2
Jérémie Piazza <jeremiepiazza@sfr.fr>
2022-03-17 15:07:30 +0100

    resolve conflits

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@
 import { Route, Routes } from 'react-router-dom';
 import LoginDashboard from './components/LoginDashboard';
 import './App.css';
-import Gameone from '../src/Gameone'
+import Gameone from './pages/Gameone'
 import UserProvider from './contexts/UserProvider';
 import logoBlanc from './assets/logoBlanc.png';
 
 function App() {
   return (
     <div className="App">
-      <Gameone />
+      
       <h1><img className='Title' src={logoBlanc}/></h1>
       <UserProvider>
         <Routes>
           <Route path='/' element={<LoginDashboard />} />
-        </Routes>
-      </UserProvider>
-      <UserProvider>
-        <Routes>
-          <Route path='/' element={<LoginDashboard />} />
+        
+          <Route path='/Game' element={<Gameone/>} />
         </Routes>
       </UserProvider>
     </div>
--- src/pages/Gameone.jsx
+++ src/pages/Gameone.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from "react";
 import ImageMapper from "react-image-mapper";
-import risque1 from './pictures/risque1.png';
+import risque1 from '../pictures/risque1.png';
 
 
 
